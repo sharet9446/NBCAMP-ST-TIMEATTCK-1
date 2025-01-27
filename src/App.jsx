05f7db559e0332ceb267b985eb2581a4903de434
sharet9446 <spees97@naver.com>
2025-01-27 12:45:38 +0900

    수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,56 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [countrylist, setCountrylist] = useState([]);
+  const [country, setCountry] = useState("");
+  const [gold, setGold] = useState("");
+  const [silver, setSilver] = useState("");
+  const [bronse, setBronse] = useState("");
 
-  return <></>;
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setCountrylist({ ...countrylist, country, gold, silver, bronse });
+  }
+
+  function handleClick() {
+    setCountry("");
+    setGold("");
+    setSilver("");
+    setBronse("");
+  }
+
+  return (
+    <>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>
+            나라 이름
+            <input type="text" />
+          </label>
+          <label>
+            금메달
+            <input type="number" min="0" max="99" />
+          </label>
+          <label>
+            은메달
+            <input type="number" min="0" max="99" />
+          </label>
+          <label>
+            동메달
+            <input type="number" min="0" max="99" />
+          </label>
+          <button type="submit">제출</button>
+          <button type="button">수정</button>
+        </form>
+        {/* <ul>
+          {setCountrylist.map((item) => (
+            <li>{item.country}</li>
+          ))}
+        </ul> */}
+      </div>
+    </>
+  );
 }
 
 export default App;
